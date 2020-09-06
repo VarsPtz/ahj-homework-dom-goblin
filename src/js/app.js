e204bae311583d64c6da7e8788b36fb6be3384ad
Alexandr Varygin <otz-job@mail.ru>
2020-09-06 09:33:43 +0300

    add Badges

--- src/js/app.js
+++ src/js/app.js
@@ -4,36 +4,35 @@ class GameBoard {
     this.previousIndexCell = 0;
     this.currentIndexCell = 0;
   }
-  
+
   drawBoard() {
     const board = document.getElementById('board');
     for (let i = 0; i < this.boardSize; i += 1) {
-     const itemBoard = document.createElement('div');
-     itemBoard.className = 'cell';
-     itemBoard.id = `cell${i}`;
-     board.appendChild(itemBoard);
+      const itemBoard = document.createElement('div');
+      itemBoard.className = 'cell';
+      itemBoard.id = `cell${i}`;
+      board.appendChild(itemBoard);
     }
 
     this.randomImg();
   }
 
   randomImg() {
-   setInterval(() => {
-    do {
-       this.currentIndexCell = Math.floor(Math.random() * this.boardSize);
-    } while (this.currentIndexCell === this.previousIndexCell);
+    setInterval(() => {
+      do {
+        this.currentIndexCell = Math.floor(Math.random() * this.boardSize);
+      } while (this.currentIndexCell === this.previousIndexCell);
 
-    if (this.previousIndexCell >= 0) {
-      const previousCell = document.getElementById(`cell${this.previousIndexCell}`);
-      previousCell.innerHTML = '';
-    }
+      if (this.previousIndexCell >= 0) {
+        const previousCell = document.getElementById(`cell${this.previousIndexCell}`);
+        previousCell.innerHTML = '';
+      }
 
-    const currentCell = document.getElementById(`cell${this.currentIndexCell}`);
-    currentCell.innerHTML = '<img src = "./img/goblin.png">';
-    this.previousIndexCell = this.currentIndexCell;
-   }, 1000);
-  }  
-
+      const currentCell = document.getElementById(`cell${this.currentIndexCell}`);
+      currentCell.innerHTML = '<img src = "./img/goblin.png">';
+      this.previousIndexCell = this.currentIndexCell;
+    }, 1000);
+  }
 }
 
 const newBoard = new GameBoard(4);
